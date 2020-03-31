3d245a4b448b5d445b00c84c44e5123ce8a0d533
SkinCrab <alanjock464@gmail.com>
2020-03-31 17:36:49 +0800

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,49 @@
+﻿from nonebot import on_notice, NoticeSession, CommandSession
+import os
+import random
+import PIL
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+from .valid_code import do
+import time
+import signal
+from nonebot import on_command
+import os.path
+
+# 处理成员增加
+msg=None
+code=None
+from_id=None
+flag=False
+@on_notice('group_increase')
+async def _(session: NoticeSession):
+    global code
+    # 欢迎入群
+    await session.send('[CQ:at,qq='+str(session.event.user_id)+'] Welcome！')
+    code=do(str(session.event.user_id))
+    await session.send('[CQ:image,file=file:///C:\\code\\'+str(session.event.user_id)+'.png]')
+    await session.send('请输入图中验证码证明你不是机器人\r\n请注意 请按照\"Code\"+图上内容 进行回答（例如 Code 12345）')
+    global from_id,flag
+    from_id=str(session.event.user_id)
+    flag=False
+
+@on_command('验证码', aliases=("Code"),only_to_me=False)
+async def _(session: CommandSession):
+    global msg,code,flag
+    if str(session.event.user_id)==str(from_id):
+        if flag==False:
+            msg = session.current_arg_text.strip()
+            msg=str(msg).lower()
+            code=str(code).lower()
+            if msg==code:
+                await session.send('恭喜你逃过一劫，欢迎来到本群！')
+                flag=True
+                code=None
+            else :
+                await session.bot.set_group_kick(user_id=session.event.user_id,group_id=session.event.group_id)
+                await session.send("很遗憾，这位朋友被s移出了群聊")
+                flag=True
+                code=None
+
+
---
+++ valid_code.py
@@ -0,0 +1,75 @@
+import os
+import random
+import PIL
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+# 生成随机底色 (r,g,b)
+
+def rand_color():
+    r=random.randint(0, 255)
+    g=random.randint(0, 255)
+    b=random.randint(0, 255)
+    return r,g,b
+
+# 新建一个120*35的图片，底色随机
+def create_picture(width=150,height=35):
+    image = Image.new('RGB', (width, height), rand_color())
+    return image
+
+def rand_str():
+    rand_num=str(random.randint(0,9))# 生成随机数字
+    rand_Lower=chr(random.randint(97,122))# 生成随机小写字母
+    rand_Upper=chr(random.randint(65,90))#生成随机大写字母
+    rand_char=random.choice([rand_num,rand_Lower,rand_Upper])#三者取其一
+    return rand_char
+
+def draw_letter(num,image,font_size):
+    draw = ImageDraw.Draw(image)
+    font_file = os.path.join('.\skincrab\plugins\JoinGroupVerification\Courier-New.ttf')
+    font = ImageFont.truetype(font_file, size=font_size)
+    temp = []
+    for i in range(num):
+        random_char = rand_str()
+        a=random_char
+        draw.text((10 + i * 30, -2), a, rand_color(), font=font)
+        temp.append(a)
+
+    valid_str= "".join(temp)  # 验证码
+    return valid_str,image
+
+def noise(image, width=120, height=35, line_count=3, point_count=20):
+    '''
+
+    :param image: 图片对象
+    :param width: 图片宽度
+    :param height: 图片高度
+    :param line_count: 线条数量
+    :param point_count: 点的数量
+    :return:
+    '''
+    draw = ImageDraw.Draw(image)
+    for i in range(line_count):
+        x1 = random.randint(0, width)
+        x2 = random.randint(0, width)
+        y1 = random.randint(0, height)
+        y2 = random.randint(0, height)
+        draw.line((x1, y1, x2, y2), fill=rand_color())
+
+        # 画点
+        for i in range(point_count):
+            draw.point([random.randint(0, width), random.randint(0, height)], fill=rand_color())
+            x = random.randint(0, width)
+            y = random.randint(0, height)
+            draw.arc((x, y, x + 4, y + 4), 0, 90, fill=rand_color())
+
+    return image
+
+def do(name):
+    image = create_picture()
+    valid_str, image = draw_letter(5, image, 30)
+    image = noise(image)
+    image.save('C://code/'+str(name)+'.png')
+    return valid_str
+
